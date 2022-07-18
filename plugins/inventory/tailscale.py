1206db1fc886d6504f84b403904cc5af48849100
Edward Muller <me@freeformz.me>
2022-07-18 10:40:33 -0700

    Updates to get things working

--- plugins/inventory/tailscale.py
+++ plugins/inventory/tailscale.py
@@ -15,7 +15,7 @@ DOCUMENTATION = """
         plugin:
             description: token to ensure this is a source file for the 'tailscale' plugin.
             required: True
-            choices: ['tailscale']
+            choices: ['freeformz.ansible.tailscale']
         include_self:
             description: Include this system in the output?
             type: bool
@@ -39,7 +39,7 @@ DOCUMENTATION = """
 """
 
 EXAMPLES = """
-plugin: tailscale
+plugin: freeformz.ansible.tailscale
 include_self: false
 ansible_host: ipv4
 strip_tag: true
@@ -48,8 +48,10 @@ os_groups: true
 
 from ansible.plugins.inventory import BaseInventoryPlugin, Constructable, Templar
 
+
 def strip_tag(tag):
     return tag.removeprefix("tag:")
+
 
 def tailscale_cli():
     tailscale_cmd = "tailscale"
@@ -57,17 +59,21 @@ def tailscale_cli():
         tailscale_cmd = "/Applications/Tailscale.app/Contents/MacOS/Tailscale"
     return tailscale_cmd
 
+
 def tailscale_status():
-    proc = subprocess.run([tailscale_cli(), "status", "--json"],capture_output=True)
+    proc = subprocess.run([tailscale_cli(), "status", "--json"], capture_output=True)
     return json.loads(proc.stdout)
 
+
 def tailscale_ipv4(host):
-    proc = subprocess.run([tailscale_cli(), "ip", "-4",host],capture_output=True)
+    proc = subprocess.run([tailscale_cli(), "ip", "-4", host], capture_output=True)
     return proc.stdout.strip().decode("utf-8")
 
+
 def tailscale_ipv6(host):
-    proc = subprocess.run([tailscale_cli(), "ip", "-6",host],capture_output=True)
+    proc = subprocess.run([tailscale_cli(), "ip", "-6", host], capture_output=True)
     return proc.stdout.strip().decode("utf-8")
+
 
 class InventoryModule(BaseInventoryPlugin, Constructable):
 
@@ -89,7 +95,7 @@ class InventoryModule(BaseInventoryPlugin, Constructable):
         "ExitNode": "exit_node",
         "ExitNodeOption": "exit_node_option",
         "Active": "active",
-        "sshHostKeys":"ssh_host_keys",
+        "sshHostKeys": "ssh_host_keys",
         "InNetworkMap": "in_network_map",
         "InMagicSock": "in_magic_sock",
         "InEngine": "in_engine",
@@ -98,7 +104,7 @@ class InventoryModule(BaseInventoryPlugin, Constructable):
         "Capabilities": "capabilities",
     }
 
-    NAME = "tailscale"
+    NAME = "freeformz.ansible.tailscale"
 
     def __init__(self):
         super(InventoryModule, self).__init__()
@@ -109,7 +115,7 @@ class InventoryModule(BaseInventoryPlugin, Constructable):
         all_hosts = list(
             tailscale_json["Peer"].values(),
         )
-        if self.config.get("include_self",False):
+        if self.config.get("include_self", False):
             all_hosts.append(
                 tailscale_json["Self"],
             )
@@ -119,38 +125,40 @@ class InventoryModule(BaseInventoryPlugin, Constructable):
             if host_name not in self.inventory.hosts:
                 self.inventory.add_host(host_name)
                 ipv4 = tailscale_ipv4(host_name)
-                self.inventory.set_variable(host_name, 'ipv4', ipv4)
+                self.inventory.set_variable(host_name, "ipv4", ipv4)
                 ipv6 = tailscale_ipv6(host_name)
-                self.inventory.set_variable(host_name, 'ipv6', ipv6)
+                self.inventory.set_variable(host_name, "ipv6", ipv6)
 
-                ansible_host = self.config.get("ansible_host","ipv4")
+                ansible_host = self.config.get("ansible_host", "ipv4")
                 if ansible_host == "ipv4":
-                    self.inventory.set_variable(host_name, 'ansible_host', ipv4)
+                    self.inventory.set_variable(host_name, "ansible_host", ipv4)
                 elif ansible_host == "ipv6":
-                    self.inventory.set_variable(host_name, 'ansible_host', ipv6)
+                    self.inventory.set_variable(host_name, "ansible_host", ipv6)
                 elif ansible_host == "dns":
-                    self.inventory.set_variable(host_name, 'ansible_host', h["DNSName"])
-                
+                    self.inventory.set_variable(host_name, "ansible_host", h["DNSName"])
+
                 for variable in self.VARIABLE_MAPPINGS:
                     if variable in h:
-                        self.inventory.set_variable(host_name, self.VARIABLE_MAPPINGS[variable], h[variable])
+                        self.inventory.set_variable(
+                            host_name, self.VARIABLE_MAPPINGS[variable], h[variable]
+                        )
 
             state = "online" if h["Online"] else "offline"
             if state not in self.inventory.groups:
                 self.inventory.add_group(state)
-            self.inventory.add_host(host_name,state)
+            self.inventory.add_host(host_name, state)
 
             if self.config.get("os_groups", True):
                 os = h["OS"]
                 if os not in self.inventory.groups:
                     self.inventory.add_group(os)
-                self.inventory.add_host(host_name,os)
+                self.inventory.add_host(host_name, os)
 
             if "Tags" in h:
                 tags = h["Tags"]
                 if self.config.get("strip_tag", True):
                     tags = list(map(strip_tag, tags))
-                self.inventory.set_variable(host_name, 'tags', tags)
+                self.inventory.set_variable(host_name, "tags", tags)
                 for tag in h["Tags"]:
                     safe_tag = tag.replace(":", "_")
                     if safe_tag not in self.inventory.groups:
@@ -159,17 +167,17 @@ class InventoryModule(BaseInventoryPlugin, Constructable):
 
     def verify_file(self, path):
         """
-            :param path: the path to the inventory config file
-            :return the contents of the config file
+        :param path: the path to the inventory config file
+        :return the contents of the config file
         """
         if super(InventoryModule, self).verify_file(path):
-            if path.endswith(('tailscale.yaml','tailscale.yml')):
+            if path.endswith(("tailscale.yaml", "tailscale.yml")):
                 return True
         return False
-    
+
     def parse(self, inventory, loader, path, cache=True):
         super(InventoryModule, self).parse(inventory, loader, path, cache)
-        
+
         self.config = self._read_config_data(path)
         self.loader = loader
         self.inventory = inventory
